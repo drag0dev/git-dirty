49e2139f1483189dc10f686835f3b1b0718d9279
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-05-08 19:28:02 +0200

    parsing the index

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,24 @@ fn main() {
     let path = if let Some(p) = args { p } else { "./".to_string() };
 
     let index = WalkDir::new(path);
+    for entry in index {
+        if let Ok(entry) = entry {
+            let path = entry.path().to_string_lossy();
+            let meta = entry.metadata();
+            if let Err(e) = meta.as_ref() {
+                println!("error reading metadata for {}: {}", path, e);
+                continue;
+            }
+
+            let meta = meta.unwrap();
+
+            // only open the directories that end with .git
+            if meta.is_dir() && path.ends_with(".git") {
+                println!("{}", path);
+            }
+
+        } else {
+            println!("Error reading file a file: {}", entry.err().unwrap());
+        }
+    }
 }
