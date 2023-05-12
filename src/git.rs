069ecb6724431b666a62453d7f1b6ce58eb1932b
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-05-12 10:45:18 +0200

    filter for workind directory statuses

--- src/git.rs
+++ src/git.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use git2::Status;
 
 pub fn test_repo(dir: &str) -> Result<()> {
     let repo = git2::Repository::open(dir).unwrap();
@@ -15,6 +16,36 @@ pub fn test_repo(dir: &str) -> Result<()> {
         collected_statuses.push((path, status));
     }
 
-    println!("{:?}", collected_statuses);
+    filter_index_statuses(&collected_statuses);
     Ok(())
+}
+
+pub fn filter_index_statuses(statuses: &Vec<(String, Status)>) {
+    let mut new = Vec::new();
+    let mut modified = Vec::new();
+    let mut deleted = Vec::new();
+    let mut typechange = Vec::new();
+    let mut renamed = Vec::new();
+    for status in statuses {
+        match status.1 {
+            Status::WT_NEW => new.push(status),
+            Status::WT_MODIFIED => modified.push(status),
+            Status::WT_DELETED => deleted.push(status),
+            Status::WT_TYPECHANGE => typechange.push(status),
+            Status::WT_RENAMED => renamed.push(status),
+            _ => {}
+        }
+    }
+
+    if new.len() == 0 && modified.len() == 0 && deleted.len() == 0 &&
+        typechange.len() == 0 && renamed.len() == 0 {
+        println!("No dirty files in the working directory");
+    } else {
+        println!("Dirty files in the working directory:");
+        for status in new { println!("New: {}", status.0); }
+        for status in modified { println!("Modified: {}", status.0); }
+        for status in deleted { println!("Deleted: {}", status.0); }
+        for status in typechange { println!("Typechange: {}", status.0); }
+        for status in renamed { println!("Renamed: {}", status.0); }
+    }
 }
